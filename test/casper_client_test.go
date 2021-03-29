7a18e3f45bf294f668501072669f6d74d678c90c
flynn <fujun.jiang@hoo.com>
2021-03-29 14:00:39 +0800

    add some rpc method

--- test/casper_client_test.go
+++ test/casper_client_test.go
@@ -7,8 +7,13 @@ import (
 	"testing"
 )
 
+const (
+	eventStoreApi = "https://event-store-api-clarity-delta.make.services"
+	RpcUrl        = "https://node-clarity-delta.make.services/rpc"
+)
+
 var (
-	casper = client.New("http://173.212.247.216:7777/rpc", "", "")
+	casper = client.New(RpcUrl, eventStoreApi)
 )
 
 func Test_GetStatus(t *testing.T) {
@@ -31,7 +36,7 @@ func Test_GetLatestBlockInfo(t *testing.T) {
 }
 
 func Test_GetBlockInfoByHeight(t *testing.T) {
-	block, err := casper.GetBlockInfoByHeight(18719)
+	block, err := casper.GetBlockInfoByHeight(18584)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -40,10 +45,15 @@ func Test_GetBlockInfoByHeight(t *testing.T) {
 }
 
 func Test_GetBlockInfoByHash(t *testing.T) {
-	block, err := casper.GetBlockInfoByHash("8eb9a991af69f92be6f92f1ff3cf93345af550ff82b602805a1cb9fc0a8fbde5")
+	block, err := casper.GetBlockInfoByHash("6f168ef1d9bfcca97146b4925924e9594dba03a3fe30952653867ecc5fda5746")
 	if err != nil {
 		t.Fatal(err)
 	}
 	d, _ := json.Marshal(block)
 	fmt.Println(string(d))
+}
+
+func Test_GetDeployByDeployHash(t *testing.T) {
+	txid := "20f1190d4ddc06246e07d5fd0454d90f3b509936e3d2584350239104e183a000"
+	casper.GetDeployByHash(txid)
 }
